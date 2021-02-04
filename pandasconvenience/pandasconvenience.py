769d5d414dd6ee985d9692410bf3d309370c52c4
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-02-03 16:34:13 -0800

    Created import_any function.

--- pandasconvenience/pandasconvenience.py
+++ pandasconvenience/pandasconvenience.py
@@ -6,3 +6,11 @@ def read_sql(query: str, driver: str, server: str, database: str) -> pd.DataFram
     conn = dbc.connect(f"Driver={driver};Server={server};Database={database};Trusted_Connection=yes;")
     return pd.read_sql(query, conn)
 
+def import_any(value: str, **kwargs) -> pd.DataFrame:
+    """Creates a data frame from a csv file, excel file or sql query."""
+    if value.endswith('.csv'):
+        return pd.read_csv(value, **kwargs)
+    elif value.endswith('.xls') or value.endswith('.xlsx'):
+        return pd.read_excel(value, **kwargs)
+    else:
+        return read_sql(value, **kwargs)
