e65642c8eef297c981aa6e20456fd7b7289d71c3
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-02-03 16:55:54 -0800

    Reformat code.

--- pandasconvenience/pandasconvenience.py
+++ pandasconvenience/pandasconvenience.py
@@ -1,11 +1,13 @@
 import pandas as pd
-from pandas import ExcelWriter
 import pyodbc as dbc
+from pandas import ExcelWriter
+
 
 def read_sql(query: str, driver: str, server: str, database: str) -> pd.DataFrame:
     """Returns a dataframe from a sql query"""
     conn = dbc.connect(f"Driver={driver};Server={server};Database={database};Trusted_Connection=yes;")
     return pd.read_sql(query, conn)
+
 
 def import_any(value: str, **kwargs) -> pd.DataFrame:
     """Creates a data frame from a csv file, excel file or sql query."""
@@ -16,12 +18,14 @@ def import_any(value: str, **kwargs) -> pd.DataFrame:
     else:
         return read_sql(value, **kwargs)
 
+
 def export_to_excel(data_frames: {pd.DataFrame}, path):
     """Exports multiple data frames into an excel workbook. The dict key is the sheet name."""
     with ExcelWriter(path) as writer:
         for key, data_frame in data_frames.items():
             data_frame.to_excel(writer, key, index=False)
         writer.save()
+
 
 def split(data_frame: pd.DataFrame, columns: [str]) -> {pd.DataFrame}:
     """Splits a data frame into multiple by the columns."""
