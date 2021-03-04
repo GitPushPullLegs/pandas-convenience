7e675ad236aa0a3f5fa732185bfd6fa6ba459bce
Aguilar.Jo <aguilar.jo@monet.k12.ca.us>
2021-03-04 14:38:20 -0800

    Updated distribution.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pandasconvenience',
-    version='0.0.1',
+    version='0.0.2',
     description='A convenience package for Pandas.',
     url='https://github.com/GitPushPullLegs/pandasconvenience',
     author='Joe Aguilar',
